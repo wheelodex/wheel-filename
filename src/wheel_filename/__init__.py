03ce69e2360460866695c6129cbfe616c7a83315
John T. Wodder II <git@varonathe.org>
2021-06-11 21:10:54 -0400

    Go black

--- src/wheel_filename/__init__.py
+++ src/wheel_filename/__init__.py
@@ -19,39 +19,39 @@ exceptions:
 Visit <https://github.com/jwodder/wheel-filename> for more information.
 """
 
-__version__      = '1.3.0'
-__author__       = 'John Thorvald Wodder II'
-__author_email__ = 'wheel-filename@varonathe.org'
-__license__      = 'MIT'
-__url__          = 'https://github.com/jwodder/wheel-filename'
+__version__ = "1.3.0"
+__author__ = "John Thorvald Wodder II"
+__author_email__ = "wheel-filename@varonathe.org"
+__license__ = "MIT"
+__url__ = "https://github.com/jwodder/wheel-filename"
 
 __all__ = [
-    'InvalidFilenameError',
-    'ParsedWheelFilename',
-    'parse_wheel_filename',
+    "InvalidFilenameError",
+    "ParsedWheelFilename",
+    "parse_wheel_filename",
 ]
 
 import os
 import os.path
 import re
-from   typing import Iterable, List, NamedTuple, Optional, Union
+from typing import Iterable, List, NamedTuple, Optional, Union
 
 # These patterns are interpreted with re.UNICODE in effect, so there's probably
 # some character that matches \d but not \w that needs to be included
-PYTHON_TAG_RGX   = r'[\w\d]+'
-ABI_TAG_RGX      = r'[\w\d]+'
-PLATFORM_TAG_RGX = r'[\w\d]+'
+PYTHON_TAG_RGX = r"[\w\d]+"
+ABI_TAG_RGX = r"[\w\d]+"
+PLATFORM_TAG_RGX = r"[\w\d]+"
 
 WHEEL_FILENAME_CRGX = re.compile(
-    r'(?P<project>[A-Za-z0-9](?:[A-Za-z0-9._]*[A-Za-z0-9])?)'
-    r'-(?P<version>[A-Za-z0-9_.!+]+)'
-    r'(?:-(?P<build>[0-9][\w\d.]*))?'
-    r'-(?P<python_tags>{0}(?:\.{0})*)'
-    r'-(?P<abi_tags>{1}(?:\.{1})*)'
-    r'-(?P<platform_tags>{2}(?:\.{2})*)'
-    r'\.[Ww][Hh][Ll]'
-    .format(PYTHON_TAG_RGX, ABI_TAG_RGX, PLATFORM_TAG_RGX)
+    r"(?P<project>[A-Za-z0-9](?:[A-Za-z0-9._]*[A-Za-z0-9])?)"
+    r"-(?P<version>[A-Za-z0-9_.!+]+)"
+    r"(?:-(?P<build>[0-9][\w\d.]*))?"
+    r"-(?P<python_tags>{0}(?:\.{0})*)"
+    r"-(?P<abi_tags>{1}(?:\.{1})*)"
+    r"-(?P<platform_tags>{2}(?:\.{2})*)"
+    r"\.[Ww][Hh][Ll]".format(PYTHON_TAG_RGX, ABI_TAG_RGX, PLATFORM_TAG_RGX)
 )
+
 
 class ParsedWheelFilename(NamedTuple):
     project: str
@@ -63,14 +63,14 @@ class ParsedWheelFilename(NamedTuple):
 
     def __str__(self) -> str:
         if self.build:
-            fmt = '{0.project}-{0.version}-{0.build}-{1}-{2}-{3}.whl'
+            fmt = "{0.project}-{0.version}-{0.build}-{1}-{2}-{3}.whl"
         else:
-            fmt = '{0.project}-{0.version}-{1}-{2}-{3}.whl'
+            fmt = "{0.project}-{0.version}-{1}-{2}-{3}.whl"
         return fmt.format(
             self,
-            '.'.join(self.python_tags),
-            '.'.join(self.abi_tags),
-            '.'.join(self.platform_tags),
+            ".".join(self.python_tags),
+            ".".join(self.abi_tags),
+            ".".join(self.platform_tags),
         )
 
     def tag_triples(self) -> Iterable[str]:
@@ -81,7 +81,7 @@ class ParsedWheelFilename(NamedTuple):
         for py in self.python_tags:
             for abi in self.abi_tags:
                 for plat in self.platform_tags:
-                    yield '-'.join([py, abi, plat])
+                    yield "-".join([py, abi, plat])
 
 
 def parse_wheel_filename(
@@ -99,17 +99,17 @@ def parse_wheel_filename(
     if not m:
         raise InvalidFilenameError(basename)
     return ParsedWheelFilename(
-        project       = m.group('project'),
-        version       = m.group('version'),
-        build         = m.group('build'),
-        python_tags   = m.group('python_tags').split('.'),
-        abi_tags      = m.group('abi_tags').split('.'),
-        platform_tags = m.group('platform_tags').split('.'),
+        project=m.group("project"),
+        version=m.group("version"),
+        build=m.group("build"),
+        python_tags=m.group("python_tags").split("."),
+        abi_tags=m.group("abi_tags").split("."),
+        platform_tags=m.group("platform_tags").split("."),
     )
 
 
 class InvalidFilenameError(ValueError):
-    """ Raised when an invalid wheel filename is encountered """
+    """Raised when an invalid wheel filename is encountered"""
 
     filename: str
 
@@ -118,4 +118,4 @@ class InvalidFilenameError(ValueError):
         self.filename = filename
 
     def __str__(self) -> str:
-        return 'Invalid wheel filename: ' + repr(self.filename)
+        return "Invalid wheel filename: " + repr(self.filename)
--- test/test_class.py
+++ test/test_class.py
@@ -1,13 +1,14 @@
 from wheel_filename import ParsedWheelFilename
 
+
 def test_pwf_iterable() -> None:
     pwf = ParsedWheelFilename(
-        project       = "project",
-        version       = "1.2.3",
-        build         = "1",
-        python_tags   = ["py2", "py3"],
-        abi_tags      = ["none"],
-        platform_tags = ["any"],
+        project="project",
+        version="1.2.3",
+        build="1",
+        python_tags=["py2", "py3"],
+        abi_tags=["none"],
+        platform_tags=["any"],
     )
     project, version, build, pytags, abi_tags, platform_tags = pwf
     assert project == "project"
